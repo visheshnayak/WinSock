aa6940aa1ce97ffbdbcf2e3f168107d1e2d6bc3d
Vishesh Nayak <visheshn@AD.NSEIT.COM>
2019-01-02 11:38:36 +0530

    Added stable Server code

--- Server/Server/Source.cpp
+++ Server/Server/Source.cpp
@@ -11,6 +11,8 @@
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
+#pragma comment(lib, "Ws2_32.lib")
+
 /*
 	STEPS TO CREATE SERVER:
 		1. Initialize Winsock.
@@ -38,6 +40,13 @@ int __cdecl main(void)
 	int recvbuflen = DEFAULT_BUFLEN;
 
 	//Initialize Winsock
+	/*
+		WSAStartup(): Initiates the use of the winsock DLL by a process
+
+		Arguments:
+			WORD wVersionRequired	:	sets the version of the socket to be created.
+			LPWSADATA lpWSAData		:	gets the feedback of the windows sockets implementation.
+	*/
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
 	{
@@ -56,7 +65,13 @@ int __cdecl main(void)
 	//Resolve the server address and ports
 	/*
 		getaddrinfo(): returns protocol independant translation from an ANSI hostname to ip addr
-		In simple words, converts the IP address to machine readable language.
+		In simple words, converts the IP address to machine readable language and gets info about host.
+
+		Arguments :
+			PCSTR pNodeName			:	contains the host name or numeric host address.
+			PCSTR pServiceName		:	contains the service name or the port number.
+			const ADDRINFOA *pHints	:	provides hints about the type of socket.
+			PADDRINFOA *ppResult	:	gets the response information about the host.
 	*/
 	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 	if (iResult != 0)
@@ -69,6 +84,11 @@ int __cdecl main(void)
 	//Create a SOCKET for listening to incoming requests
 	/*
 		socket(): creates an unbound socket and returns the file descriptor that can be later used to operate on the sockets.
+
+		Arguments:
+			int af			:	specifies the communication in which the SOCKET is to be created.
+			int type		:	specifies the type of SOCKET to be created.
+			int protocol	:	specifies the protocol to be used in SOCKET.
 	*/
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (ListenSocket == INVALID_SOCKET)
@@ -81,7 +101,12 @@ int __cdecl main(void)
 
 	//Bind the SOCKET for TCP connection
 	/*
-		bind(): binds the socket with the address and length
+		bind(): associates a local address with a SOCKET.
+
+		Arguments:
+			SOCKET s				:	descriptor of an unbound SOCKET.
+			const sockaddr *addr	:	address of host.
+			int namelen				:	length, in bytes, of the value.
 	*/
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR)
@@ -100,11 +125,15 @@ int __cdecl main(void)
 	//Listen for incoming connection for the client socket
 	/*
 		listen(): function places a socket in a state of listening for incoming connection.
+
+		Arguments:
+			SOCKET s	:	descriptor of unbound, unconnected SOCKET.
+			int backlog	:	maximum length of queue for pending connection.
 	*/
 	iResult = listen(ListenSocket, SOMAXCONN);
 	if (iResult == SOCKET_ERROR)
 	{
-		printf("listen faield with error: %d\n", WSAGetLastError());
+		printf("listen failed with error: %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
@@ -112,7 +141,12 @@ int __cdecl main(void)
 
 	//Accept a client socket
 	/*
-		accept(): accepts an incoming connection and is associated with an socket immediately.
+		accept(): permits an incoming connection attempt on a SOCKET.
+
+		Arguments:
+			SOCKET s		:	descriptor of SOCKET in listening state.
+			sockaddr *addr	:	optional pointer to address of connecting entity.
+			int *addrlen	:	optional pointer to length of structure of addr parameter.
 	*/
 	ClientSocket = accept(ListenSocket, NULL, NULL);
 	if (ClientSocket == INVALID_SOCKET)
@@ -129,7 +163,13 @@ int __cdecl main(void)
 	do
 	{
 		/*
-			recv() function recieves the data from the connected socket.
+			recv(): recieves the data from the connected socket or a bound connectionless socket.
+
+			Arguments:
+				SOCKET s	:	descriptor for the SOCKET.
+				char *buf	:	buffer to recieve the incoming data.
+				int len		:	length of the buffer.
+				int flags	:	for the modes of function.
 		*/
 		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 		if (iResult > 0)
@@ -138,7 +178,13 @@ int __cdecl main(void)
 
 			//Echo the buffer back to sender
 			/*
-				send(): sends data to a connected socket
+				send(): sends data to a connected SOCKET.
+
+				Arguments:
+					SOCKET s		:	descriptor identifying a connected SOCKET.
+					const char *buf	:	buffer containing the data to be transmitted.
+					int len			:	length of data in the buffer.
+					int flags		:	specifies the way in which the call is made.
 			*/
 			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
 			if (iSendResult == SOCKET_ERROR)
@@ -164,6 +210,13 @@ int __cdecl main(void)
 	} while (iResult > 0);
 
 	//shutdown the connection
+	/*
+		shutdown(): disables sends or recieves on a SOCKET.
+
+		Arguments;
+			SOCKET s	:	descriptor identifying a SOCKET.
+			int how		:	flag that describes what types of operation will be stopped.
+	*/
 	iResult = shutdown(ClientSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR)
 	{
